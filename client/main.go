4797a5a6901f268c0cd5fbcaefb9976cc2cee522
Muhammad Fajar Febriano <fajarfebriano@gmail.com>
2022-11-07 12:52:50 +0700

    fix attribut of object

--- client/main.go
+++ client/main.go
@@ -39,14 +39,14 @@ func main() {
 
 	log.Printf("Hit todoSvc.List\n")
 	todo, _ := todoSvc.List(ctx, new(empty.Empty))
-	log.Printf("List Todo %+v\n", todo.GetList())
+	log.Printf("List Todo %+v\n", todo.GetData())
 
 	log.Printf("Hit todoSvc.Remove %+v\n", todo1)
 	_, _ = todoSvc.Remove(ctx, todo1)
 
 	log.Printf("Hit todoSvc.List after remove\n")
 	todoAfterDelete, _ := todoSvc.List(ctx, new(empty.Empty))
-	log.Printf("List Todo after remove %+v\n", todoAfterDelete.GetList())
+	log.Printf("List Todo after remove %+v\n", todoAfterDelete.GetData())
 
 	log.Printf("Hit todoSvc.Edit %+v\n", todo2)
 	todo2.Name = "Udah di edit deh !"
@@ -54,5 +54,5 @@ func main() {
 
 	log.Printf("Hit todoSvc.List after edit\n")
 	todoAfterEdit, _ := todoSvc.List(ctx, new(empty.Empty))
-	log.Printf("List Todo after edit %+v\n", todoAfterEdit.GetList())
+	log.Printf("List Todo after edit %+v\n", todoAfterEdit.GetData())
 }
